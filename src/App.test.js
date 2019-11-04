967e46d09b3e35af929e9c46537954b99f3bd44b
Matthieu MARTIN <ginshuto@MATTHIEU.localdomain>
2019-11-04 16:46:31 +0100

    tentative test couleur

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TimerOne from './components/TimerOne';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+it('Couleur en fonction du nombre de secondes restantes', () =>{
+  const div = document.createElement(TimerOne());
+  ReactDOM.render(<App/>, div);
+  const test = TimerOne();
+  test.setSeconds(30);
+  expect(this.state.Class).toBe("blue");
+})
